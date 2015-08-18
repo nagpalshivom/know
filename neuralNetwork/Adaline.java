43c7d406c3903407c0ec46c7f57602cc3d94a4d8
asoc <om.nagpal@utradesolutions.com>
2015-08-18 18:49:30 +0530

    changes in adaline neural network sample

--- neuralNetwork/Adaline.java
+++ neuralNetwork/Adaline.java
@@ -3,61 +3,103 @@ class AdalineNN{
     double w1, w2, b, err;
     int [][] inp;
     int [] out;
-    String gate;
-    public AdalineNN(double w1, double w2, double b, int [][] inp, int [] out, String gate) {
+    //constructor of the class AdalineNN which is used to initialize
+    //weights and input, output matrix
+    public AdalineNN(double w1, double w2, double b, int [][] inp, int [] out) {
         this.w1 = w1;
         this.w2 = w2;
         this.b = b;
         this.inp = inp;
         this.out = out;
         this.err = 0;
-        this.gate = gate;
     }
+    //the function below trains the neural network for desired epochs
+    //and prints new calulated weights
     public void trainNN(int epochs) {
         for(int i = 1;i <= epochs;i++) {
             System.out.println("epoch : " + i);
             this.err = 0;
-            trainNNEpoch();
+            this.trainNNEpoch();
             this.printNNInfo();
         }
+        //after training it checks whether the weights satisfy OR gate
+        //condition
+        if(this.checkNN())
+            System.out.println("final weights satisfy \"OR\" gate");
+        else
+            System.out.println("final weights don't satisfy \"OR\" gate");
     }
+    //the function below trains the neural network for a single epoch
     private void trainNNEpoch() {
-        System.out.println("in train epoch");
         double y, deltaW1, deltaW2, tempErr;
         for(int i = 0;i < 4;i++) {
             y = this.b + inp[0][i] * this.w1 + inp[1][i] * this.w2;
-            tempErr = (out[i] - y);
-            deltaW1 = tempErr * alpha * inp[0][i];
-            deltaW2 = tempErr * alpha * inp[1][i];
+            tempErr = (this.out[i] - y);
+            deltaW1 = tempErr * alpha * this.inp[0][i];
+            deltaW2 = tempErr * alpha * this.inp[1][i];
             this.w1 += deltaW1;
             this.w2 += deltaW2;
             this.b += (tempErr * alpha);
-            err += (tempErr * tempErr);
+            this.err += (tempErr * tempErr);
         }
     }
+    //activation function used to check the final weights
+    private int actFn(double y) {
+        if(y >= 0)
+            return 1;
+        else
+            return -1;
+    }
+    //checks whether final weights comply with OR gate
     private boolean checkNN() {
         int i;
         double y;
         for(i = 0;i < 4;i++) {
-            y = b + inp[0][i] * w1 + inp[1][i] * w2;
-            if(y != out[i])
+            y = this.b + this.inp[0][i] * this.w1 + this.inp[1][i] * this.w2;
+            if(this.actFn(y) != this.out[i])
                 return false;
         }
         return true;
     }
+    //prints info at the end of each epoch
     public void printNNInfo() {
-        System.out.println(gate + "-gate\n" + "W1 = " + w1 + "\nW2 = " + w2 + "\nBias = " + b + "\nerr : " + err + "\n");
+        System.out.println("W1 = " + this.w1 + "\nW2 = " + this.w2 + "\nBias = " + this.b + "\nerr : " + this.err + "\n");
     }
 }
 public class Adaline {
     public static void main(String [] args) {
+        int epochs;
         int [][] inp = {{1, 1, -1, -1}, {1, -1, 1, -1}};
-        int [] andOut = {1, -1, -1, -1};
         int [] orOut = {1, 1, 1, -1};
-        double w1 = 0.5, w2 = 0.5, b = 0.5;
-        //AdalineNN andNN = new AdalineNN(w1, w2, b, inp, andOut, "and");
-        AdalineNN orNN = new AdalineNN(w1, w2, b, inp, orOut, "or");
-        //andNN.trainNN();
-        orNN.trainNN(100);
+        double w1, w2, b;
+        if(args.length != 4) {
+            System.out.println("Usage : java Adaline w1 w2 bias epoch");
+            return;
+        }
+        try {
+            epochs = Integer.parseInt(args[3]);
+        }
+        catch(NumberFormatException nfe) {
+            System.out.println("Epoch value should be a natural number");
+            return;
+        }
+        if(epochs <= 0) {
+            System.out.println("Epoch value should be a natural number");
+            return;
+        }
+        try {
+        w1 = Double.parseDouble(args[0]);
+        w2 = Double.parseDouble(args[1]);
+        b = Double.parseDouble(args[2]);
+        }
+        catch(NumberFormatException nfe) {
+            System.out.println("W1, W2 and Bias should be real number");
+            return;
+        }
+        //creating a neural network for or gate
+        AdalineNN orNN = new AdalineNN(w1, w2, b, inp, orOut);
+        //initializing with weights and input, output values for training
+        //function below starts the training
+        orNN.trainNN(epochs);
     }
 }
