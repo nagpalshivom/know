21bbd505e97f78afa4094c51904dae45d2e80691
nagpalshivom <nagpalshivom@gmail.com>
2020-11-03 13:36:37 +0530

    accepted round 316 div 2 D

--- codeforces/round316Div2_D.java
+++ codeforces/round316Div2_D.java
@@ -1,190 +1,193 @@
 import java.util.*;
 import java.io.*;
 
-class round316Div2_D {
-    public static void main(String[] args) {
-        MyScanner sc = new MyScanner();
-        out = new PrintWriter(new BufferedOutputStream(System.out));
+public class round316Div2_D implements Runnable {
+
+    static Vector<Vector<Integer>> nodeChildren;
+    static Vector<Vector<int[]>> heightBasedTimeAndLetterCountPair;
+    static int[] in;
+    static int[] out;
+    static int t;
+    static String chars;
+
+    public static PrintWriter outWriter;
+    public static MyScanner sc;
+
+    public static void main(String[] args) throws InterruptedException {
+        sc = new MyScanner();
+        outWriter = new PrintWriter(new BufferedOutputStream(System.out));
+        Thread t = new Thread(null, new round316Div2_D(), "round316Div2_D", 1 << 30);
+        t.start();
+        t.join();
+    }
+
+    @Override
+    public void run() {
 
         int n, m;
         n = sc.nextInt();
         m = sc.nextInt();
-        List<Node> nodes = new ArrayList<Node>(n);
-        List<Node> levelEndMarkers = new ArrayList<Node>(n);
+        nodeChildren = new Vector<Vector<Integer>>(n + 1);
+        in = new int[n + 1];
+        out = new int[n + 1];
+        t = 1;
+        for(int i = 0;i <= n;i++) {
+            nodeChildren.add(new Vector<Integer>());
+        }
 
 
-        nodes.set(0, new Node(0));
-        levelEndMarkers.set(0, nodes.get(0));
-        for(int i = 1;i < n; n++) {
+        for(int i = 2;i <= n;i++) {
             int p = sc.nextInt();
-            Node nodep = nodes.get(p - 1);
-            nodes.set(i, new Node(i));
-            Node nodei = nodes.get(i);
-
-            nodep.children.add(nodei);
-            nodei.height = nodep.height + 1;
-            Node heightPredecessor = levelEndMarkers.get(nodei.height);
-            if(heightPredecessor != null) {
-                nodei.prev = heightPredecessor;
-            }
-            heightPredecessor = nodei;
+            nodeChildren.get(p).add(i);
+        }
+        if(n == 1) {
+            sc.nextLine();
         }
 
-        String chars = sc.nextLine();
-        for(int i = 0;i < n;i++) {
-            nodes.get(i).c = chars.charAt(i);
-        }
+        chars = sc.nextLine();
 
-        populateMaxHeight(nodes.get(0));
-        populateLetterCoundTillNow(levelEndMarkers);
+        heightBasedTimeAndLetterCountPair = new Vector<Vector<int[]>>(n + 1);
+        for(int i = 0;i < n + 1;i++) {
+            heightBasedTimeAndLetterCountPair.add(new Vector<int[]>());
+        }
+        
+        dfs(1, 1);
+
         for(int i = 0;i < m;i++) {
+            String ans = "Yes";
             int v = sc.nextInt();
             int h = sc.nextInt();
-            Node nodev = nodes.get(v - 1);
-            int l = findLeftmostEdgeVertexAtHeight(nodev, h);
-            int r = findRightmostEdgeVertexAtHeight(nodev, h);
-            int bitForL = nodes.get(l).letterCountTillNow;
-            int bitForR = nodes.get(r).letterCountTillNow;
-            int charAtR = nodes.get(r).c - 'a';
-            int odd = 0;
-            for(int j = 0;j < 26 && odd < 2;j++) {
-                if(((bitForL & (1 << j)) & ((bitForR | (1 << charAtR)) & (1 << j))) > 0) {
-                    odd++;
-                }
-            }
-            String ans = "Yes";
-            if(odd > 1) {
-                ans = "No";
-            }
-            out.write(ans);
-        }
 
-        out.close();
-    }
-
-    static int findLeftmostEdgeVertexAtHeight(Node root, int height) {
-        if(root == null || root.height > height) {
-            return -1;
-        } else {
-            if(root.height == height) {
-                return root.v;
-            } else {
-                int answerFromChild = -1;
-                for(Node child : root.children) {
-                    answerFromChild = findLeftmostEdgeVertexAtHeight(child, height);
-                    if(answerFromChild != -1) {
-                        return answerFromChild;
+            int r;
+            int l = getLeftEdge(heightBasedTimeAndLetterCountPair, in[v], out[v], h);
+            if(l != -1) {
+                r = getRightEdge(heightBasedTimeAndLetterCountPair, in[v], out[v], h);
+                if(r != -1) {
+                    int ansToCheck = l ^ r;
+                    if((ansToCheck & (ansToCheck - 1)) != 0) {
+                        ans = "No";
                     }
                 }
-                return answerFromChild;
             }
+            outWriter.write(ans + "\n");
         }
+
+
+        outWriter.close();
     }
 
-    static int findRightmostEdgeVertexAtHeight(Node root, int height) {
-        if(root == null || root.height > height) {
-            return -1;
-        } else {
-            if(root.height == height) {
-                return root.v;
-            } else {
-                int answerFromChild = -1;
-                Iterator<Node> descIterator = root.children.descendingIterator();
-                while(descIterator.hasNext()) {
-                    Node child = descIterator.next();
-                    answerFromChild = findRightmostEdgeVertexAtHeight(child, height);
-                    if(answerFromChild != -1) {
-                        return answerFromChild;
-                    }
-                }
-                return answerFromChild;
+    static int getRightEdge(Vector<Vector<int[]>> heightBasedTimeAndLetterCountPair, int in, int out, int h) {
+        int probAns = -1;
+        Vector<int[]> heightBasedTimeAndLetterCountPairForH = heightBasedTimeAndLetterCountPair.get(h);
+        int s = 0;
+        int e = heightBasedTimeAndLetterCountPairForH.size() - 1;
+        int m = (s + e) / 2;
+        while(s <= e && m >= 0) {
+            int[] pair = heightBasedTimeAndLetterCountPairForH.get(m);
+            if(pair[0] < out && pair[0] > in) {
+                s = m + 1;
+                probAns = pair[1];
+            } else if(pair[0] > out) {
+                e = m - 1;
+            } else if(pair[0] < in) {
+                s = m + 1;
             }
+            m = (s + e) / 2;
         }
+        return probAns;
     }
 
-    static void populateLetterCoundTillNow(List<Node> levelEndMarkers) {
-        for(Node levelEndMarker : levelEndMarkers) {
-            int letterCountTillNow = 0;
-            while(levelEndMarker != null) {
-                int bitToSet = levelEndMarker.c - 'a';
-                letterCountTillNow = letterCountTillNow | 1 << bitToSet;
-                levelEndMarker.letterCountTillNow = letterCountTillNow;
-                levelEndMarker = levelEndMarker.prev;
+    static int getLeftEdge(Vector<Vector<int[]>> heightBasedTimeAndLetterCountPair, int in, int out, int h) {
+        int[] probAns = new int[] {0, 0, -1};
+        Vector<int[]> heightBasedTimeAndLetterCountPairForH = heightBasedTimeAndLetterCountPair.get(h);
+        int s = 0;
+        int e = heightBasedTimeAndLetterCountPairForH.size() - 1;
+        int m = (s + e) / 2;
+        while(s <= e && m >= 0) {
+            int[] pair = heightBasedTimeAndLetterCountPair.get(h).get(m);
+            if(pair[0] > in && pair[0] < out) {
+                e = m - 1;
+                probAns = pair;
+            } else if(pair[0] < in) {
+                s = m + 1;
+            } else if(pair[0] > out) {
+                e = m - 1;
             }
+            m = (s + e) / 2;
         }
+        return probAns[1] ^ probAns[2];
     }
 
-    static int populateMaxHeight(Node root) {
-        //root == null will never occur (hopefully)
-        if(root == null) {
-            return -1;
-        } else {
-            for(Node child : root.children) {
-                int prospectiveMaxHeight = populateMaxHeight(child) + 1;
-                if(root.maxheight < prospectiveMaxHeight) {
-                    root.maxheight = prospectiveMaxHeight;
-                }
-            }
-            return root.maxheight;
+    static void dfs(int v, int h) {
+        in[v] = t;
+        t = t + 2;
+        for(int u : nodeChildren.get(v)) {
+            dfs(u, h + 1);
         }
+        int charBit = 1 << (chars.charAt(v - 1) - 'a');
+        int charBitTillNow = charBit;
+        if(!heightBasedTimeAndLetterCountPair.get(h).isEmpty()) {
+            charBitTillNow = charBit ^ heightBasedTimeAndLetterCountPair.get(h).lastElement()[1];
+        }
+        heightBasedTimeAndLetterCountPair.get(h).add(new int[] {in[v] + 1, charBitTillNow, charBit});
+        out[v] = t++;
     }
+
 
     public static class Node { 
-        Node prev;
-        char c;
         int v;
-        int maxheight;
-        int height;
-        int letterCountTillNow;
-        LinkedList<Node> children = new LinkedList<Node>();
+        Vector<Node> children = new Vector<Node>();
+        int in;
+        int out;
+        int h;
 
-        public Node(int v) {
+        public Node(int v, int h) {
             this.v = v;
+            this.h = h;
         }
     }
 
-    public static PrintWriter out;
-      
-   public static class MyScanner {
-      BufferedReader br;
-      StringTokenizer st;
- 
-      public MyScanner() {
-         br = new BufferedReader(new InputStreamReader(System.in));
-      }
- 
-      String next() {
-          while (st == null || !st.hasMoreElements()) {
-              try {
-                  st = new StringTokenizer(br.readLine());
-              } catch (IOException e) {
-                  e.printStackTrace();
-              }
-          }
-          return st.nextToken();
-      }
- 
-      int nextInt() {
-          return Integer.parseInt(next());
-      }
- 
-      long nextLong() {
-          return Long.parseLong(next());
-      }
- 
-      double nextDouble() {
-          return Double.parseDouble(next());
-      }
- 
-      String nextLine(){
-          String str = "";
-	  try {
-	     str = br.readLine();
-	  } catch (IOException e) {
-	     e.printStackTrace();
-	  }
-	  return str;
-      }
 
-   }
+    public static class MyScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public MyScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine(){
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+
+    }
 }
